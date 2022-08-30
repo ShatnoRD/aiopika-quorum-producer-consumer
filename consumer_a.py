79c662a5ae66fd4529842c4ef430f873e334a992
ShatnoRD <antalmate99@gmail.com>
2022-08-30 14:18:49 +0300

    initial commit

---
+++ consumer_a.py
@@ -0,0 +1,46 @@
+import asyncio
+import aio_pika
+
+import os
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())
+AMQP_ADDRESS = os.environ.get("AMQP_ADDRESS")
+EXCHANGE_NAME = os.environ.get("EXCHANGE_NAME")
+BINDING_KEYS = [os.environ.get("BINDING_KEY_A"), os.environ.get("BINDING_KEY_B")]
+QUEUE_NAME = "consumer_a"
+
+
+async def receive_amqp_messages():
+    print("Connecting to message")
+    connection: aio_pika.robust_connection.RobustConnection = await aio_pika.connect_robust(AMQP_ADDRESS)
+    async with connection:
+        channel: aio_pika.channel.Channel = await connection.channel()
+
+        # declaring exchange
+        exchange: aio_pika.exchange.Exchange = await channel.declare_exchange(
+            EXCHANGE_NAME,
+            aio_pika.ExchangeType.TOPIC,
+            durable=True,
+        )
+
+        # declaring queue
+        queue: aio_pika.queue.Queue = await channel.declare_queue(
+            QUEUE_NAME,
+            durable=True,
+            # arguments={"x-queue-type": "quorum"},
+        )
+
+        # binding queue to exchange
+        for topic in BINDING_KEYS:
+            await queue.bind(exchange, routing_key=topic)
+
+        # listen
+        async with queue.iterator() as queue_iter:
+            async for message in queue_iter:
+                async with message.process():
+                    print("Message body is: %r" % message.body)
+
+
+if __name__ == "__main__":
+    asyncio.run(receive_amqp_messages())
---
+++ producer.py
@@ -0,0 +1,63 @@
+import json
+import time
+import asyncio
+import aio_pika
+
+import os
+from random import randint
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())
+AMQP_ADDRESS = os.environ.get("AMQP_ADDRESS")
+EXCHANGE_NAME = os.environ.get("EXCHANGE_NAME")
+BINDING_KEYS = [os.environ.get("BINDING_KEY_A"), os.environ.get("BINDING_KEY_B")]
+
+
+async def emit(channel: aio_pika.channel.Channel, route: str, data: dict):
+    # declaring exchange
+    exchange: aio_pika.exchange.Exchange = await channel.declare_exchange(
+        EXCHANGE_NAME,
+        aio_pika.ExchangeType.TOPIC,
+        durable=True,
+    )
+    # Sending the message
+    message_body = json.dumps(data)
+    message = aio_pika.Message(message_body.encode())
+    await exchange.publish(message, routing_key=route)
+
+
+async def emit_amqp_messages(channel: aio_pika.channel.Channel):
+    for topic in BINDING_KEYS:
+        for _ in range(randint(1, 5)):
+            await emit(channel, topic, f"payload sent for topic[{topic}]")
+
+
+async def retry_if_connection_fails(retries: int, delay: int):
+    # A custom retry loop
+    for i in range(retries):
+        try:
+            # connect to server
+            connection: aio_pika.robust_connection.RobustConnection = await aio_pika.connect_robust(AMQP_ADDRESS)
+            channel: aio_pika.channel.Channel = await connection.channel()
+
+            # send a ramdom number of predefined messages to given conenction
+            emit_amqp_messages(channel)
+
+            # close connection
+            await connection.close()
+
+            # if everything went down without exceptions, exit
+            return
+
+        except Exception as e:
+            if i < retries - 1:
+                time.sleep(delay)
+                print(e)
+                continue
+            else:
+                print("failed to emit {data} \n amqp producer out of connection retries")
+                raise
+
+
+if __name__ == "__main__":
+    asyncio.run(retry_if_connection_fails())
